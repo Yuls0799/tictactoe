bdfc4cb88d717c8d23c31c70617715f1a5831e9a
Yuliana Zorrilla <107286036+Yuls0799@users.noreply.github.com>
2024-08-02 10:50:54 -0400

    Changes in structure

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Esperar 5 segundos antes de redirigir
+    setTimeout(() => {
+        window.location.href = 'game.html'; // Cambia la ruta según sea necesario
+    }, 3000); // 5000 milisegundos = 5 segundos
+});
